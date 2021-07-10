52d314e7f21fde9f80600bcde9b5847eb3e4e5cc
faryz-coder <faryz.coder@gmail.com>
2021-07-10 08:05:32 +0800

    finish create ads

--- app/src/main/java/com/deventhirran/carrental/CreateAds.kt
+++ app/src/main/java/com/deventhirran/carrental/CreateAds.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.provider.MediaStore
 import android.util.Log.d
 import androidx.fragment.app.Fragment
@@ -13,7 +15,14 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.view.isGone
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
 import com.deventhirran.carrental.databinding.FragmentCreateAdsBinding
+import com.deventhirran.carrental.ui.user.viewmodel.LoginViewModel
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 
 
 class CreateAds : Fragment() {
@@ -22,6 +31,8 @@ class CreateAds : Fragment() {
 
     private val binding get() = _binding!!
     private var imageUri : Uri? = null
+    private val storage = Firebase.storage
+    private val db = Firebase.firestore
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,12 +45,64 @@ class CreateAds : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        var viewModel = ViewModelProvider(requireActivity()).get(LoginViewModel::class.java)
+        d("debugCreateAds", "debugCreateAds: id:${viewModel.id}")
         binding.buttonConfirm.setOnClickListener {
             // Check form
             if (valid()) {
                 d("bomoh", "form is valid")
                 // [UPLOAD] Data
+                //upload image
+                val data = hashMapOf(
+                    "title" to binding.editListingName.text.toString(),
+                    "charge" to binding.editHourlyRate.text.toString()
+                )
+                db.collection("ads")
+                    .add(data)
+                    .addOnSuccessListener {
+                        val documentId = it.id
+                        // UPLOAD IMAGE
+                        val storageRef = storage.reference.child(documentId)
+                        val uploadImg = storageRef.putFile(imageUri!!)
+
+                        val urlTask = uploadImg.continueWithTask { task ->
+                            if (!task.isSuccessful) {
+                                task.exception?.let {
+                                    throw it
+                                }
+                            }
+                            storageRef.downloadUrl
+                        }.addOnCompleteListener { task ->
+                            if (task.isSuccessful) {
+                                val imgUrl = task.result.toString()
+                                val imgData = hashMapOf(
+                                    "image" to imgUrl
+                                )
+                                db.collection("ads").document(documentId)
+                                    .set(imgData, SetOptions.merge())
+                                    .addOnSuccessListener {
+                                        // [ADD TO USER DB]
+                                        val data2 = hashMapOf(
+                                            "title" to binding.editListingName.text.toString(),
+                                            "charge" to binding.editHourlyRate.text.toString(),
+                                            "image" to imgUrl
+                                        )
+                                        db.collection("user").document(viewModel.id.toString()).collection("Post")
+                                            .add(data2)
+                                            .addOnSuccessListener {
+                                                Toast.makeText(requireContext(), "Ads Posted!", Toast.LENGTH_SHORT).show()
+                                                Handler(Looper.getMainLooper()).postDelayed({
+                                                    requireView().findNavController().popBackStack()
+                                                }, 1200)
+                                            }
+                                            .addOnFailureListener { e ->
+                                                d("debugCreateAds", "debugCreateAds:Error: $e")
+                                            }
+                                    }
+                            }
+                        }
+
+                    }
 
             }
         }
